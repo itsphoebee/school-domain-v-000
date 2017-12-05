11d1fa4f94320644429406279f98a588bf0f3bd2
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 19:40:35 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,9 +9,9 @@ attr_reader :school_name, :roster
   end
 
   def add_student(name, grade)
-    roster[grade] = []
-    roster[grade] << name
-    roster
+    @roster[grade] = []
+    @roster[grade] << name
+    @roster
   end
 
 end
