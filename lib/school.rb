07ee843f38d7e00919bc1159fdaa52c516eb6435
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 19:50:24 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -12,6 +12,8 @@ attr_reader :school_name, :roster
     if @roster[grade] == nil
       @roster[grade] = []
       @roster[grade] << name
+    elsif @roster[grade].length > 0
+      @roster[grade] << name
     end
     @roster
   end
