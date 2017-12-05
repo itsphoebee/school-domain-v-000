b82ef70bd7abde573efb7d04b2d2effe2551b013
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 20:29:01 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -9,9 +9,9 @@ attr_reader :school_name, :roster
   end
 
   def add_student(name, grade)
-    @roster[grade]||= []
-    @roster[grade] << name
-    @roster
+    @roster[grade]||= []      #ONLY IF @roster[grade] is undefined/nil/false, assign it to []
+    @roster[grade] << name    #then add name into the hash
+    @roster                   #return the roster
   end
 
   def grade(grade)
