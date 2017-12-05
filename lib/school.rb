96653ed4449e0dca3453476d9a7e7fc6e0643d64
Phoebe Lau <phoebelau7@gmail.com>
2017-12-05 20:00:59 +0000

    automatically backed up by learn

--- lib/school.rb
+++ lib/school.rb
@@ -10,7 +10,12 @@ attr_reader :school_name, :roster
 
   def add_student(name, grade)
 
-      @roster[grade] || @roster[grade] << name
+    if @roster[grade] == nil
+      @roster[grade] = []
+      @roster[grade] << name
+    elsif @roster[grade].length > 0
+      @roster[grade] << name
+    end
     @roster
   end
 
@@ -19,5 +24,6 @@ attr_reader :school_name, :roster
   end
 
   def sort
-
+    @roster.sort_by!{|a| a[:grade][name]
+    }
 end
